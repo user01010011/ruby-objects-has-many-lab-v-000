cf347a5854bdfcb0a12623e424bd0e4f15dbf0a3
Shasha Yan <shashayan@gmail.com>
2021-05-17 18:13:27 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,27 @@
+class Author
+  attr_accessor :name
+
+  @@all = [ ]
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def self.post_count
+    post.all.select{|post|post.author == self}
+  end
+end
