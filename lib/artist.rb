156f1439057917dfccfcf598268089efd617378f
Shasha Yan <shashayan@gmail.com>
2021-05-17 18:14:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,24 @@
 class Artist
+
   attr_accessor :name
+  @@all = [ ]
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all?
   end
 
   def add_song(song)
     song.artist = self
   end
 
-  def add_song_by_name(name, artist)
-    song = Song.new(name, artist)
-    add_song(song)
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
   end
 
   def self.song_count
